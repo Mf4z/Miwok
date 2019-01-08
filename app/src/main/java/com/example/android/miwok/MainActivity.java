b191e2a974b9e13462f2a6a5bd486a4f3b264eb3
Marwan <emef4z@gmail.com>
2019-01-08 15:26:25 +0100

    Add onClick method to all categories

--- app/src/main/java/com/example/android/miwok/MainActivity.java
+++ app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,8 +15,10 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +28,29 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+    }
+
+    public void openNumbersList(View view){
+
+        Intent intent = new Intent(this,NumbersActivity.class);
+        startActivity(intent);
+    }
+
+    public void openColorsList(View view){
+
+        Intent intent = new Intent(this,ColorsActivity.class);
+        startActivity(intent);
+    }
+
+    public void openPhrasesList(View view){
+
+        Intent intent = new Intent(this,PhrasesActivity.class);
+        startActivity(intent);
+    }
+
+    public void openFamilyMembersList(View view){
+
+        Intent intent = new Intent(this,FamilyMembersActivity.class);
+        startActivity(intent);
     }
 }
