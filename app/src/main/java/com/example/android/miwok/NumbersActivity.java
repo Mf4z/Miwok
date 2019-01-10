240316a9097a8bcb6b3e928b028aaa4865a7d9c7
Marwan <emef4z@gmail.com>
2019-01-10 17:44:17 +0100

    Add array of numbers and using log messages

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,7 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +10,24 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        //Array of Numbers
+
+        String[] engNumbers = new String[10];
+        engNumbers[0] = "One";
+        engNumbers[1] = "Two";
+        engNumbers[2] = "Three";
+        engNumbers[3] = "Four";
+        engNumbers[4] = "Five";
+        engNumbers[5] = "Six";
+        engNumbers[6] = "Seven";
+        engNumbers[7] = "Eight";
+        engNumbers[8] = "Nine";
+        engNumbers[9] = "Ten";
+
+        Log.v("NumbersActivity","Number : "+engNumbers[0]);
+        Log.v("NumbersActivity","Number : "+engNumbers[1]);
+
     }
+
 }
