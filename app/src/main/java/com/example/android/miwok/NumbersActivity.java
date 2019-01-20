9149b26f39e2a61398d74f5410157fb8e9882ccd
Marwan <emef4z@gmail.com>
2019-01-20 12:32:03 +0100

    Add ArrayList to Numbers Activity

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -1,8 +1,10 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -11,23 +13,31 @@ public class NumbersActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
 
-        //Array of Numbers
+      //Create A List of Numbers
+        ArrayList<String> words = new ArrayList<String>();
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
 
-        String[] engNumbers = new String[10];
-        engNumbers[0] = "One";
-        engNumbers[1] = "Two";
-        engNumbers[2] = "Three";
-        engNumbers[3] = "Four";
-        engNumbers[4] = "Five";
-        engNumbers[5] = "Six";
-        engNumbers[6] = "Seven";
-        engNumbers[7] = "Eight";
-        engNumbers[8] = "Nine";
-        engNumbers[9] = "Ten";
 
-        Log.v("NumbersActivity","Number : "+engNumbers[0]);
-        Log.v("NumbersActivity","Number : "+engNumbers[1]);
-
+        // Verify the contents of the list by printing out each element to the logs
+        Log.v("NumbersActivity", "Word at index 0: " + words.get(0));
+        Log.v("NumbersActivity", "Word at index 1: " + words.get(1));
+        Log.v("NumbersActivity", "Word at index 2: " + words.get(2));
+        Log.v("NumbersActivity", "Word at index 3: " + words.get(3));
+        Log.v("NumbersActivity", "Word at index 4: " + words.get(4));
+        Log.v("NumbersActivity", "Word at index 5: " + words.get(5));
+        Log.v("NumbersActivity", "Word at index 6: " + words.get(6));
+        Log.v("NumbersActivity", "Word at index 7: " + words.get(7));
+        Log.v("NumbersActivity", "Word at index 8: " + words.get(8));
+        Log.v("NumbersActivity", "Word at index 9: " + words.get(9));
     }
 
 }
